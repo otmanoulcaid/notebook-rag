452324ab459b64cad06c1abcb64befd349e57753
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-12-14 13:00:40 +0000

    course materials

--- rag/app/src/main/java/org/mql/genai/rag/controllers/ChatAssistantController.java
+++ rag/app/src/main/java/org/mql/genai/rag/controllers/ChatAssistantController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 @RestController
 @RequestMapping("/api/chat")
 public class ChatAssistantController {
