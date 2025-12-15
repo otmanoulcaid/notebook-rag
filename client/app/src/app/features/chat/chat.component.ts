7c802d89073708df146ce457380fdc0a3fbef197
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-12-15 20:19:05 +0000

    bot thinking animation

--- client/app/src/app/features/chat/chat.component.ts
+++ client/app/src/app/features/chat/chat.component.ts
@@ -1,40 +1,63 @@
 import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../../services/chat.service';
+import { Message } from '../../models/Message';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class ChatComponent {
-  messages = [
-    { sender: 'bot', text: 'Hello! I am your Note Assistant. How can I help you today?' },
-    { sender: 'user', text: 'Hi! Can I ask questions about my PDFs?' },
-    { sender: 'bot', text: 'Absolutely! Upload a PDF and then ask me anything about it.' }
+
+  messages: Message[] = [
+    { sender: 'bot', text: 'Hello! I am your Note Assistant. How can I help you today?' }
   ];
-  @ViewChild('chatWindow') private chatWindow!: ElementRef;
+
+  newMessage = '';
   chatService = inject(ChatService);
-  newMessage: string = '';
+
+  @ViewChild('chatWindow') private chatWindow!: ElementRef;
 
   sendMessage() {
     if (!this.newMessage.trim()) return;
 
-    this.messages.push({ sender: 'user', text: this.newMessage });
-    this.chatService.send(this.newMessage).subscribe({
-      error: err=> console.log(err),
-      next: (response) => this.messages.push({ sender: 'bot', text: response.response })
-    })
+    this.messages.push({
+      sender: 'user',
+      text: this.newMessage
+    });
+
+    const thinkingMessage: Message = {
+      sender: 'bot',
+      text: '',
+      loading: true
+    };
+
+    this.messages.push(thinkingMessage);
+
+    const question = this.newMessage;
     this.newMessage = '';
+
+    this.chatService.send(question).subscribe({
+      next: (response) => {
+        thinkingMessage.loading = false;
+        thinkingMessage.text = response.response;
+      },
+      error: () => {
+        thinkingMessage.loading = false;
+        thinkingMessage.text = '⚠️ Sorry, something went wrong.';
+      }
+    });
   }
 
   ngAfterViewChecked() {
     this.scrollToBottom();
   }
 
-  private scrollToBottom(): void {
+  private scrollToBottom() {
     if (this.chatWindow) {
       const el = this.chatWindow.nativeElement;
       el.scrollTop = el.scrollHeight;
